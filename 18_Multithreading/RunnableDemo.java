34523374d5c389f39f44906bcba196d40d24bcd4
LeNgocBao145 <himagito@gmail.com>
2025-10-14 19:26:20 +0700

    feat: Practice using method sleep() and join() of class Thread

--- 18_Multithreading/RunnableDemo.java
+++ 18_Multithreading/RunnableDemo.java
@@ -8,16 +8,32 @@ class RunnableDemo implements Runnable{
     public void run(){
         for(int i = 1; i <= 10; i++){
             System.out.println(i);
+            try{
+                Thread.sleep(600, 500); // sleep(ms, ns) => sleep for ms (miliseconds) + ns (nanoseconds)
+            }catch(Exception e){
+                e.printStackTrace();
+            }
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Runnable r1 = new RunnableDemo();
         Thread t1 = new Thread(r1);
         t1.start();
+
+        // Method join() is used to wait 
+        // for a thread to finish its execution
+        // t1.join(); // wait for t1 to finish for indefinite time
+
+        t1.join(2000); // wait for t1 to finish just for 2 seconds
+
         Runnable r2 = new RunnableDemo();
         Thread t2 = new Thread(r2);
         t2.start();
+
+        Runnable r3 = new RunnableDemo();
+        Thread t3 = new Thread(r3);
+        t3.start();
 
         // Normal method
         // This will not use multithreading
