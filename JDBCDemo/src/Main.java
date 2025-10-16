9dde18a9774eed4ec8210c4035ca02811c767cc8
LeNgocBao145 <himagito@gmail.com>
2025-10-16 18:38:03 +0700

    feat: Practice using Spring Boot Initilizr to create a template of Spring project

---
+++ JDBCDemo/src/Main.java
@@ -0,0 +1,14 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main
+{
+	public static void main(String[] args) {
+		List<String> l = new ArrayList<>();
+		l.add("hello");
+		String res = l.get(0);
+                
+		System.out.println(res);
+	}
+}
