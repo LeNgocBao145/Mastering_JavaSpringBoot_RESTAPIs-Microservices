4b0b7e127afb20a623bf8ffd09bb9352ef735a06
LeNgocBao145 <himagito@gmail.com>
2025-10-16 19:05:19 +0700

    feat: Create simple RESTful API Controller Application

---
+++ RestApp/src/main/java/com/example/rest/controller/MyController.java
@@ -0,0 +1,19 @@
+package com.example.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MyController {
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "<h1 style='color:red'>Hello World<h1/>";
+	}
+	
+	@GetMapping("/hi")
+	public String sayHi() {
+		return "<h1 style='color:blue'>Hi Java<h1/>";
+	}
+}
