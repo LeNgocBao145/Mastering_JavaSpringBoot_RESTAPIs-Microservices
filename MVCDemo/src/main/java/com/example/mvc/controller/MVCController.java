d92f28f9a223d19da474709b8e9b85f76ce82d05
LeNgocBao145 <himagito@gmail.com>
2026-02-02 10:22:20 +0700

    feat: mvc demo with SSR

---
+++ MVCDemo/src/main/java/com/example/mvc/controller/MVCController.java
@@ -0,0 +1,12 @@
+package com.example.mvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MVCController {
+	@GetMapping
+	public String welcome() {
+		return "welcome.html";
+	}
+}
