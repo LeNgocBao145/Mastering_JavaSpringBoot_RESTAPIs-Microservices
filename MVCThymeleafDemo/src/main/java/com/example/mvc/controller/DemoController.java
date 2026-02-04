f61a34c80f078d75e53b127c6d6b54edcfaaaae1
LeNgocBao145 <himagito@gmail.com>
2026-02-04 13:58:43 +0700

    feat: Spring MVC - Reading Form Data

--- MVCThymeleafDemo/src/main/java/com/example/mvc/controller/DemoController.java
+++ MVCThymeleafDemo/src/main/java/com/example/mvc/controller/DemoController.java
@@ -1,8 +1,11 @@
 package com.example.mvc.controller;
 
+import com.example.mvc.MvcThymeleafDemoApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DemoController {
@@ -10,5 +13,16 @@ public class DemoController {
 	public String getDate(Model model) {
 		model.addAttribute("date1", new java.util.Date());	
 		return "date"; 
+	}
+	
+	@GetMapping("/showForm")
+	public String showForm() {
+		return "hello-form";
+	}
+	
+	@PostMapping("/processForm")
+	public String processForm(@RequestParam("name") String name, Model model) {
+		model.addAttribute("name", name.toUpperCase());
+		return "result";
 	}
 }
